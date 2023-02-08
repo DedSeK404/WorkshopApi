a3191506a916065865fd953cefe21ac8c7ba9189
DedSeK404 <marwenbennaceur5@gmail.com>
2023-02-08 08:50:17 +0100

    third commit

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,30 +1,53 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const Details = () => {
-    const [data, setData]=useState()
-    const {idrecipe} = useParams()
-    console.log(idrecipe)
-    useEffect(()=>{
-        const getOneRecipe = async () => {
-            try {
-                const res = await axios.get(
-                  `https://api.edamam.com/api/recipes/v2/${idrecipe}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}
+  const [data, setData] = useState();
+  const { idrecipe } = useParams();
+  // console.log(idrecipe)
+  useEffect(() => {
+    const getOneRecipe = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.edamam.com/api/recipes/v2/${idrecipe}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}
                   `
-                );
-                // console.log(res.data.hits)
-                setData(res.data.recipe);
-              } catch (error) {
-                console.log(error);
-              }
-            };
-            getOneRecipe();
-        
-    },[idrecipe])
-    return(
-        <div>
-
+        );
+       //  console.log(res.data.hits)
+        setData(res.data.recipe);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getOneRecipe();
+  }, [idrecipe]);
+  //console.log(data);
+  return (
+    <div>
+      <h1>Recipe Details</h1>
+      <div style={{display:"flex", flexDirection:"column"}}>
+      
+        <div class="card">
+          <h2 class="card-title">{data && data.label}</h2>
+          <img style={{width:"100%"}}
+            src={data && data.image}
+            alt=""
+          />
+         
         </div>
-    )
-}
+        <div class="card-desc">
+          <p style={{textAlign:'center', color:'#5f1e0a',fontSize:'35px', fontWeight:"800"}}>
+        Ingredients:
+          </p>
+          
+          {data && data.ingredients.map((e)=>(
+            <div  style={{display:"flex", flexDirection:"column"}}>
+             <li >{e && e.text}</li>
+             </div>
+             
+          ))}
+          </div>
+      </div>
+      </div>
+  );
+};
